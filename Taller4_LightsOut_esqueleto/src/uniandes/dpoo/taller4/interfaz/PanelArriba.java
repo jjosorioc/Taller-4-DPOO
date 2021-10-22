1e562b1a7b17cb4239da41c5a58adcaa161cc038
Juan José Osorio <j.osorioc@uniandes.edu.co>
2021-10-22 12:19:18 -0500

    Avances en el panel de arriba

---
+++ Taller4_LightsOut_esqueleto/src/uniandes/dpoo/taller4/interfaz/PanelArriba.java
@@ -0,0 +1,15 @@
+package uniandes.dpoo.taller4.interfaz;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelArriba extends JPanel
+{
+	private VentanaPrincipal padre; // I am your father.
+	
+	public PanelArriba(VentanaPrincipal padre)
+	{
+		this.padre = padre;
+	}
+	//TODO
+}
